a27425ac68a622c909720e9444527e75364c656f
avirusiam404 <ribavan123wawa@gmail.com>
2025-09-04 14:36:41 +0000

    Better bot, now pings instead of just typing

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-блять это сканнер для пингования и поиска локалок хорошо работает на playit.gg, made for using with discord webhook
+блять это сканнер для пингования и поиска локалок хорошо работает на `playit.gg`, made for using with discord webhook
+
+> [!NOTE]
+> IM DUMB AND I HARDCODED SOME THINGS LIKE DISCORD ROLES PLS CHANGE IT IN THE `./__main__.py`
+> CURRENT VALUES:
+> `1412851881939701780` - Pinging on every server
+> `1413150208727973998` - Pinging when bot can join and there is cheats enabled
+> `1413150268266254336` - Pinging when bot can join and there is cheats disabled
+> `1413150345684848713` - Pinging when bot cant join because of kick
+
 
 > [!NOTE]
 > SET THIS TEMPLATE AND CHANGE IT
--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ from dotenv import dotenv_values as envval
 from discord_webhook import DiscordWebhook, DiscordEmbed
 from time import sleep as slp
 from javascript import require, On, Once
+import queue
 mineflayer = require('mineflayer')
 import re
 
@@ -15,7 +16,7 @@ start_port = int(input('startport: '))
 end_port = int(input('endport: '))
 
 
-def makebot(ip, port, username, embed):
+def makebot(ip, port, username, q):
     bot = mineflayer.createBot({
         "host": ip,
         "port": port,
@@ -27,41 +28,57 @@ def makebot(ip, port, username, embed):
         bot.chat('/gamemode 1')
 
     @On(bot, 'messagestr')
-    def handler_messagestr(_1, _2, _3, _4, _5):
+    def handler_messagestr(_1, _2, _3, _4, _5,*args):
         if _2 == 'Unknown or incomplete command, see below for error':
-            embed.add_embed_field(name="Success", value=f'cheats:False')
+            q.put({"name":"Cheats","value":f'<@&1413150268266254336>'})
             bot.quit()
         elif _2 in ('Incorrect argument for command', 'Set own game mode to Creative Mode'):
-            embed.add_embed_field(name="Success", value=f'cheats:True')
+            q.put({"name":"Cheats","value":f'<@&1413150208727973998>'})
             bot.quit()
+
 
     @On(bot, 'kicked')
     def handler_kicked(_1, reason, _2):
-        embed.add_embed_field(name="Kicked", value=f'{reason}')
+        q.put({"name":"Kicked","value":f'<@&1413150345684848713> {reason}'})
+
+def run_bot(ip,port,username):
+    q = queue.Queue()
+    makebot(ip,port,username,q)
+    return q
+
+def getcontent(result):
+    if result.get('name')=="Kicked": return "<@&1412851881939701780>\n<@&1413150345684848713>"
+    elif result=={"name":"Cheats","value":f'<@&1413150208727973998>'}: return "<@&1412851881939701780>\n<@&1413150208727973998>"
+    elif result=={"name":"Cheats","value":f'<@&1413150268266254336>'}: return "<@&1412851881939701780>\n<@&1413150268266254336>"
+    else: print(result)
+
 
 
 def dowebhook(port, desc, players, protocol, name):
-    webhook = DiscordWebhook(
-        url=WEBHOOK,
-        username='archx2 scanner :D',
-        content="<@NOPING&1412851881939701780>"
-    )
-    embed = DiscordEmbed(
-        title=f'{ip}:{port}',
-        description="Found a LAN Server",
-        color="03b2f8"
-    )
-    embed.set_timestamp()
-    embed.add_embed_field(name="PlayerCount", value=f"{players}/8")
-    embed.add_embed_field(name="MOTD", value=f"{desc}")
-    embed.add_embed_field(name="Version", value=f"{name}")
-
     if protocol <= 767:
-        makebot(ip, port, 'nuLL', embed)
-        slp(15)
-    else:
-        embed.add_embed_field(name="Cant check", value=f'protocol {protocol} is higher than 767, cant check')
+        q=run_bot(ip, port, 'nuLL')
+        try:
+            result = q.get(timeout=30)
+            
+            webhook = DiscordWebhook(
+                url=WEBHOOK,
+                username='archx2 scanner :D',
+                content=getcontent(result,)
+            )
+            embed = DiscordEmbed(
+                title=f'{ip}:{port}',
+                description="Found a LAN Server",
+                color="03b2f8"
+            )
+            embed.set_timestamp()
+            embed.add_embed_field(name="PlayerCount", value=f"{players}/8")
+            embed.add_embed_field(name="MOTD", value=f"{desc}")
+            embed.add_embed_field(name="Version", value=f"{name}")
 
+            embed.add_embed_field(name=result.get('name'), value=result.get('value'))
+        except queue.Empty:
+            return
+        
     webhook.add_embed(embed)
     webhook.execute()
 
@@ -72,7 +89,7 @@ def check_port(port):
         status = server.status()
         desc = status.description
         if status.players.max == 8 and " - " in desc:
-            print(f'found smth on {ip}:{port}')
+            print(f'found smth on {ip}:{port}\n')
             dowebhook(port, desc, status.players.online, status.version.protocol, status.version.name)
         else:
             pass
